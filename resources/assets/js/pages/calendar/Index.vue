761cc6342fdeb6266fd1d6117632f4d76dbe56f0
Curtis Delicata <curtis.delicata@outlook.com>
2018-05-15 23:27:05 +0100

    Route

---
+++ resources/assets/js/pages/calendar/Index.vue
@@ -0,0 +1,30 @@
+<template>
+	<div id="app">
+		<calendar-view
+			:show-date="showDate"
+			@show-date-change="setShowDate"
+			class="holiday-us-traditional holiday-us-official"
+			display-period-uom="month"
+		/>
+	</div>
+</template>
+<script>
+	import CalendarView from "vue-simple-calendar"
+	require("vue-simple-calendar/dist/static/css/default.css")
+	require("vue-simple-calendar/dist/static/css/holidays-us.css")
+
+	export default {
+		name: 'app',
+		data: function() {
+			return { showDate: new Date() }
+		},
+		components: {
+			CalendarView
+		},
+		methods: {
+			setShowDate(d) {
+				this.showDate = d;
+			},
+		}
+	}
+</script>
